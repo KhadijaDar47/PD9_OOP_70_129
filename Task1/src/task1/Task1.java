79f33986fdfbfaaddc21d3346c7717cba28d30af
NoorAtif129 <77112713+NoorAtif@users.noreply.github.com>
2021-04-24 15:51:26 +0500

    Some Changes and The last Function Is Done. Task 1 Completed.

--- Task1/src/task1/Task1.java
+++ Task1/src/task1/Task1.java
@@ -13,47 +13,56 @@ import java.util.Scanner;
  */
 public class Task1 {
 
-    public static int wordCounter(String line) {
-        int count=1;
-        for (int i = 0; i < line.length(); i++) {
-            if(line.charAt(i)==' ')
-            {
+    public static int wordCounter(String s) {
+        int count = 1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
                 count++;
             }
         }
         return count;
     }
-     public static int vowelCounter(String line) {
-        int count=0;
-        for (int i = 0; i < line.length(); i++) {
-            if(line.charAt(i)=='a'||line.charAt(i)=='e'||line.charAt(i)=='i'||line.charAt(i)=='o'||line.charAt(i)=='u')
-            {
+
+    public static int vowelCounter(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u' || (s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U')) {
                 count++;
             }
         }
         return count;
     }
-      public static int puncCounter(String line) {
-        int count=0;
-        for (int i = 0; i < line.length(); i++) {
-            if(line.charAt(i)=='.'||line.charAt(i)==','||line.charAt(i)==':'||line.charAt(i)==';'||line.charAt(i)=='?')
-            {
+
+    public static int puncCounter(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '.' || s.charAt(i) == ',' || s.charAt(i) == ':' || s.charAt(i) == ';' || s.charAt(i) == '?') {
                 count++;
             }
         }
         return count;
     }
-    
+
+    public static Boolean findSubString(String s) {
+        Boolean flag = false;
+        if (s.contains("the")) {
+            flag = true;
+            System.out.println("the is a part of the string");
+        } else {
+            System.out.println("the is not a part of the string");
+        }
+        return flag;
+    }
 
     public static void main(String[] args) {
         String line;
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the String: ");
         line = input.nextLine();
-        System.out.println("Contain words: "+wordCounter(line));
-        System.out.println("Contain Vowels: "+vowelCounter(line));
-        System.out.println("Contain Punction: "+puncCounter(line));
+        System.out.println("Number Of words: " + wordCounter(line));
+        System.out.println("Number Of Vowels: " + vowelCounter(line));
+        System.out.println("Number Of Punctions: " + puncCounter(line));
+        System.out.print("Substring: " + findSubString(line));
 
     }
-
 }
